982e97c9c3259dad1bce01241cdb05cb8e7b5dba
rbrauwers <rbrauwers@gmail.com>
2022-04-26 14:00:24 -0300

    v1.0

---
+++ samplelib/src/main/java/com/rb/samplelib/SampleLib.kt
@@ -0,0 +1,16 @@
+package com.rb.samplelib
+
+class SampleLib {
+
+    var publicVar: Int = 0
+    private var privateVar: Int = 0
+
+    private fun publicMethod() {
+
+    }
+
+    private fun privateMethod() {
+
+    }
+
+}
